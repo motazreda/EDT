34d744056b8ae7e882e356921f5668682c9157e4
motazreda <motazkhodair@gmail.com>
2017-10-16 16:36:11 +0300

    add option for variable name -v

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ if __name__ == '__main__':
 	parser.add_argument('-s', action="store", help="shellcode file <shell.bin>",type=str)
 	parser.add_argument('-c', action="store", help="chunk size", type=int)
 	parser.add_argument('-p', action="store", help="choose padding size", type=int)
+	parser.add_argument('-v', action="store", help="choose variable name", type=str)
 	res = parser.parse_args()
 	if res.s and res.c and res.p:
-		om = Omlete(chunk_size=res.c, padding=res.p, tag=["\\x12", "\\x34", "\\x56", "\\x78"], shellcode=res.s)
+		om = Omlete(chunk_size=res.c, padding=res.p, tag=["\\x12", "\\x34", "\\x56", "\\x78"], shellcode=res.s, var_name=res.v)
 		om.generate()
--- omlete.py
+++ omlete.py
@@ -1,10 +1,11 @@
 class Omlete(object):
-	
-	def __init__(self, chunk_size=None, padding=None, tag=None, shellcode=None):
+
+	def __init__(self, chunk_size=None, padding=None, tag=None, shellcode=None, var_name=None):
 		self.shellcode = shellcode
 		self.chunk_size = chunk_size
 		self.padding = ["\\x90"] * padding
 		self.tag = tag
+		self.var_name = var_name
 		print "[+] generate with tag \\x12\\x34\\x56\\x78 0x78563412\n"
 
 	def split(self, arr, size):
@@ -17,14 +18,17 @@ class Omlete(object):
 	    return arrs
 
 	def generate(self):
+		count = 0
 		try:
 			data = open(self.shellcode).read()
 		except:
 			print "Error Reading File"
 			sys.exit(0)
-
 		arrs = self.split(data, self.chunk_size)
+		if not self.var_name:
+			self.var_name = 'OMLETE'
 		for i, arr in enumerate(arrs):
+			count += 1
 			shell = []
 			shell += self.padding
 			shell += self.tag
@@ -34,5 +38,6 @@ class Omlete(object):
 				shell.append("\\x01")
 			shell.append("\\x" + str(hex(len(arr))).replace("0x",""))
 			shell += ["\\x" + arr.encode("hex")[i*2:i*2+2] for i in range(len(arr.encode('hex'))/2)]
-			print "".join(shell)
-			print "\n"
+			egg = self.var_name + str(count) + "=\""
+			egg += "".join(shell)
+			print egg + "\""
