01d4bf41899fe769b7f357e8c3f70236494ef06d
motazreda <motazkhodair@gmail.com>
2017-10-17 10:10:39 +0300

    fixing ...

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
 	# msfvenom -p windows/shell_bind_tcp -b '\x00' -f raw > shell.bin
 	banner = "[+] Exploit Development Toolkit (EDT) [+]"
 	banner += "\n"
-	banner += "[+] By motazreda https://github.com/motazreda [+]"
+	banner += "[+] By motazreda https://github.com/motazreda @dangerousmind5 [+]"
 	parser = argparse.ArgumentParser(description=banner, formatter_class=argparse.RawTextHelpFormatter)
 	parser.add_argument('-s', action="store", help="shellcode file <shell.bin>",type=str)
 	parser.add_argument('-c', action="store", help="chunk size", type=int)
